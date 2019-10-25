1455b6b68217d8ee474395275363a84d94d98ba8
Mary West <westmarye1219@gmail.com>
2019-10-25 14:19:15 -0500

    deleting item in database

--- capstoneproject/capstoneapp/views/items/details.py
+++ capstoneproject/capstoneapp/views/items/details.py
@@ -39,3 +39,24 @@ def item_details(request, item_id):
         }
 
         return render(request, template, context)
+
+    elif request.method == 'POST':
+        form_data = request.POST
+
+        # Check if this POST is for deleting a book
+        #
+        # Note: You can use parenthesis to break up complex
+        #       `if` statements for higher readability
+        if (
+            "actual_method" in form_data
+            and form_data["actual_method"] == "DELETE"
+        ):
+            with sqlite3.connect(Connection.db_path) as conn:
+                db_cursor = conn.cursor()
+
+                db_cursor.execute("""
+                DELETE FROM capstoneapp_item
+                WHERE id = ?
+                """, (item_id,))
+
+            return redirect(reverse('capstoneapp:items'))
