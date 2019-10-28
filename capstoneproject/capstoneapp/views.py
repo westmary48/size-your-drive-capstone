783b7e7ebc9aeaacb83019b7008e34d81bc3224c
Mary West <westmarye1219@gmail.com>
2019-10-28 14:16:15 -0500

    added some styling

--- capstoneproject/capstoneapp/views.py
+++ capstoneproject/capstoneapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.utils import timezone
+from .models import *
+from .render import Render
 
-# Create your views here.
+
+class Pdf(View):
+
+    def get(self, request):
+        Item = Item.objects.all()
+        today = timezone.now()
+        params = {
+            'today': today,
+            'items': items,
+            'request': request
+        }
+        return Render.render('pdf.html', params)
